cf59b67dcd4d28a01365623e4bd6e1d1ebb8400e
TranXuanPhong <tranxuanphong26525@gmail.com>
2021-10-05 22:17:28 +0700

    hmm

--- js/navigation-handler.js
+++ js/navigation-handler.js
@@ -32,66 +32,31 @@
         let dr = []
         const dropdown__toggle = document.querySelectorAll('.dropdown__toggle')
         dropdown__toggle
-        .forEach((x,i) => {
-            // x.setAttribute('data-toggle', i)
+        .forEach(x => {
+            
             const drop__containers = x.nextElementSibling
             dr.push(drop__containers)
-            x.onclick = e => {
-                dropdown__toggle
-                .forEach((y,id)=>{
-                //     if(y.classList.contains('active')&& x.getAttribute('data-toggle')==y.getAttribute('data-toggle')){
-                //         y.classList.remove('active')
-                //         y.classList.add('hidden')
-                //     }
-                if(id!=i){
-                    
-
-                    y.nextElementSibling.classList.add('hidden')
-                    y.nextElementSibling.classList.remove('active')
-                    // console.log(id,i);
-                }
-                })
+            x.onclick = function () {
                 
-                
-                    drop__containers.classList.toggle('hidden')
-                    drop__containers.classList.toggle('active')
-                
-
+                drop__containers.classList.toggle('hidden')
+    
             }
         })
         //drop:)
 
         const sub__toggle = document.querySelectorAll('.sub-toggle')
-        sub__toggle.forEach((x,i) => {
-            const a = x.firstElementChild
+        sub__toggle.forEach(x => {
+            const drop__containers = x.firstElementChild
             
             
-            dr.push(a.nextElementSibling)
+            dr.push(drop__containers.nextElementSibling)
 
-            x.onclick = e => {
-                sub__toggle
-                .forEach((y,id)=>{
-                //     if(y.classList.contains('active')&& x.getAttribute('data-toggle')==y.getAttribute('data-toggle')){
-                //         y.classList.remove('active')
-                //         y.classList.add('hidden')
-                //     }
-                
-                if(id!=i){
-                    
-
-                    y.firstElementChild.nextElementSibling.classList.add('hidden')
-                    y.firstElementChild.nextElementSibling.classList.remove('active')
-                    // console.log(id,i);
-                }
-                })
-                a.nextElementSibling.classList.toggle('hidden')
-                a.nextElementSibling.classList.toggle('active')
-                // console.log(e.target);
+            x.onclick = () => {
+                drop__containers.nextElementSibling.classList.toggle('hidden')
     
             }   
         })
         dr.forEach(x => x.classList.add('hidden'))
-        // dr.forEach(x =>x.onclick=event=>console.log(event.target))
         const search = document.querySelector('#search-box')
         const seb = document.querySelector('#b-toggle')
         const sesubmit = document.querySelector('#b-submit')
@@ -104,5 +69,6 @@
             search.classList.toggle('hidden')
             sesubmit.classList.toggle('hidden')
         }
-
+        
+    
 })()
